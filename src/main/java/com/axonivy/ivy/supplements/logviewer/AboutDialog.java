53a19598c6836844a4b4b6a3f1beecb4442b2aab
barbarabrugger <baba_hurter@gmx.net>
2018-04-17 15:23:06 +0200

    Add application icon

--- src/main/java/com/axonivy/ivy/supplements/logviewer/AboutDialog.java
+++ src/main/java/com/axonivy/ivy/supplements/logviewer/AboutDialog.java
@@ -2,6 +2,8 @@ package com.axonivy.ivy.supplements.logviewer;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 public class AboutDialog {
 	public static void showAbout() {
@@ -10,7 +12,8 @@ public class AboutDialog {
 		alert.setHeaderText(
 				"Started as a simple JavaFX test project, but is actually useful as a small and simple log viewer.");
 		alert.setContentText("(c) 2018 AXON IVY AG - pes, bhu");
-
+		Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+		stage.getIcons().add(new Image("/images/icon.png"));
 		alert.showAndWait();
 	}
 }
--- src/main/java/com/axonivy/ivy/supplements/logviewer/Main.java
+++ src/main/java/com/axonivy/ivy/supplements/logviewer/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -23,6 +24,7 @@ public class Main extends Application {
 		Scene scene = new Scene(root, 640, 320);
 
 		stage.setTitle("IvyLogViewer");
+		stage.getIcons().add(new Image("/images/icon.png"));
 		stage.setScene(scene);
 		stage.show();
 	}
